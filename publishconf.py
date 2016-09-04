5a32f2f84ed92392de97d1e29fafbd2eb5cabf36
bhuztez <bhuztez@gmail.com>
2016-09-04 21:23:14 +0800

    switch to https

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://bhuztez.github.io'
+SITEURL = 'https://bhuztez.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds.atom'
